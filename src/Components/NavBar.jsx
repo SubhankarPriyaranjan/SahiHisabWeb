54cf0900f6f65d804d631af080e4c9492848b99c
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-09-02 17:42:15 +0530

    Added Tailwind Successfully

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-      <p>Hello World</p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   )
 }
