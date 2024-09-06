7972086d63789186576995831e25b322da4c3d75
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-09-06 15:53:16 +0530

    About Us Page Added Need to

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
+import AboutUs from './Components/Pages/AboutUs'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-     <NavBar/>
-    </>
+    <Router>
+      <NavBar />
+    
+      <Routes>
+        <Route path="/about-us" element={<AboutUs />} /> {/* Create a route for About Us */}
+        <Route path="/home" element={<NavBar />} /> {/* Create a route for Home */}
+      </Routes>
+    </Router>
   )
 }
 
