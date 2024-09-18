d3c2451a697a8b23bd4b02035035ecf347f615f9
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-09-18 17:32:20 +0530

    ContactUs Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './Components/NavBar'
 import AboutUs from './Components/Pages/AboutUs'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ContactUs from './Components/Pages/ContactUs'
 
 function App() {
   
@@ -12,8 +13,9 @@ function App() {
       <NavBar />
     
       <Routes>
-        <Route path="/about-us" element={<AboutUs />} /> {/* Create a route for About Us */}
+        <Route path="/about-us" element={<AboutUs/>} /> {/* Create a route for About Us */}
         <Route path="/home" element={<NavBar />} /> {/* Create a route for Home */}
+        <Route path="/contactus" element={<ContactUs />} /> {/* Create a route for ContactUs */}
       </Routes>
     </Router>
   )
