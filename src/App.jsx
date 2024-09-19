f2baead72132bb9cd1ae65b485753ab8278e1dcf
dashbichitrananda01 <dashbichitrananda01@gmail.com>
2024-09-19 05:48:12 +0000

    login page create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css'
 import NavBar from './Components/NavBar'
 import AboutUs from './Components/Pages/AboutUs'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LogIn from './Components/Pages/LogIn'
 
 function App() {
   
@@ -10,10 +12,11 @@ function App() {
   return (
     <Router>
       <NavBar />
-    
+      {/* <LogIn /> */}
       <Routes>
         <Route path="/about-us" element={<AboutUs />} /> {/* Create a route for About Us */}
         <Route path="/home" element={<NavBar />} /> {/* Create a route for Home */}
+        <Route path="/log-in" element={<LogIn />} /> {/*  Create a route for LogIn */}
       </Routes>
     </Router>
   )
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,28 +4,32 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
         Hello world!
-      </h1>
-      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      </h1> */}
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-50 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="#" className="h-8" alt="" />
             <span className="self-center text-2xl font-bold whitespace-nowrap text-black dark:text-white">Sahi Hisab</span>
           </a>
           <div className="flex md:order-2 space-x-4 md:space-x-4 rtl:space-x-reverse border rounded-full px-4 py-2">
-            <button
-              type="button"
-              className="text-blue-800 dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <Link to="/log-in"
+              href="#"
             >
-              Log In
-            </button>
-            <button
+              <button
+                type="button"
+                className="text-blue-800 dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Log In
+              </button>
+            </Link>
+            {/* <button
               type="button"
               className="text-blue-800 dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Register
-            </button>
+            </button> */}
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
