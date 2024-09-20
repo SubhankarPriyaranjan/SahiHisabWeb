765e2d3ea269b966d5b28f806916e0642c3e8ab8
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-09-20 11:26:26 +0530

    Register Button missing Added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -24,12 +24,12 @@ function NavBar() {
                 Log In
               </button>
             </Link>
-            {/* <button
+            <button
               type="button"
               className="text-blue-800 dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Register
-            </button> */}
+            </button>
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
